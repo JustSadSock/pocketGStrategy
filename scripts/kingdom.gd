5e3e5de5347c146b23b71dcb93a3d957a8968f46
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-08 08:56:13 +0300

    Update kingdom.gd

--- scripts/kingdom.gd
+++ scripts/kingdom.gd
@@ -4,7 +4,7 @@ class_name Kingdom
 @export var name : String = "Player"
 @export var color : Color = Color(1,0.8,0)
 var gold : int = 200
-var relations : Dictionary = {}
+var relations : Dictionary = {}  # id фракции → значение (-100…+100)
 
 func add_relation(id:int, delta:int):
     relations[id] = relations.get(id, 0) + delta
